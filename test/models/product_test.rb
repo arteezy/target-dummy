0e2e878c36a0d4229cdf2e7bbb7a3a972183fa91
Arthur L <mardybm@gmail.com>
2014-11-04 07:40:30 +0600

    added new test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -28,4 +28,24 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
+
+  test "image url" do
+    ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  def new_product(image_url)
+    product = Product.new(title:        "My Book Title",
+                          description:  "yyy",
+                          price:        1,
+                          image_url:    image_url)
+  end
 end
