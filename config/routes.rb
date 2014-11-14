4c96d006669b05cdadabc421867b3159e0338e66
arteezy <mardybm@gmail.com>
2014-11-14 08:33:16 +0600

    added basic controller and CSS styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Depot::Application.routes.draw do
   get "store/index"
   resources :products
 
+  root 'store#index', as: 'store'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
