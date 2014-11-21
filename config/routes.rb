855e0a4708c79bfbed7f42ffbc70ec6e548f34c2
arteezy <mardybm@gmail.com>
2014-11-21 15:20:12 +0600

    added complete order handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Depot::Application.routes.draw do
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get "store/index"
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   root 'store#index', as: 'store'
 
