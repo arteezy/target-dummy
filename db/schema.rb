7ef7a57180177e1b8c77bf5bd40efe0c87cb1e6d
arteezy <mardybm@gmail.com>
2014-11-24 13:15:10 +0600

    added bcrypt gem for secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121104821) do
+ActiveRecord::Schema.define(version: 20141124070221) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20141121104821) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
