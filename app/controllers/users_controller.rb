ff326a845751236adb1eddf9d3d293a291498c76
arteezy <mardybm@gmail.com>
2014-11-24 14:59:16 +0600

    added tests for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User #{@user.name} successfully updated." }
+        format.html { redirect_to users_url, notice: "User #{@user.name} successfully updated." }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,10 +18,10 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { name: @user.name, password: 'secret', password_confirmation: 'secret' }
+      post :create, user: { name: 'sam', password: 'secret', password_confirmation: 'secret' }
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to users_path
   end
 
   test "should show user" do
@@ -36,7 +36,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should update user" do
     patch :update, id: @user, user: { name: @user.name, password: 'secret', password_confirmation: 'secret' }
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to users_path
   end
 
   test "should destroy user" do
