8517db833c1f572f292cff54dec1940b91be2827
arteezy <mardybm@gmail.com>
2014-11-25 11:21:55 +0600

    added code to prevent deletion of admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  after_destroy :ensure_an_admin_remains
   has_secure_password
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
 end
