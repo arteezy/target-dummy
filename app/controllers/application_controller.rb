f9bb0d7b2fb5d28c94eb17911f76561e7960a477
arteezy <mardybm@gmail.com>
2014-11-25 13:41:37 +0600

    added i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_action :set_i18n_locale_from_params
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   protected
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
 
     def authorize
       unless User.find_by(id: session[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,18 @@ Depot::Application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
 
   get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
